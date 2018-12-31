f7ad951483ab75c2e804d2559a74f4e150572ae4
Nick Doyle <nick.doyle@intelematics.com>
2018-12-31 15:56:49 +1100

    feat: move to bucket -2 and -3

--- tf/s3.tf
+++ tf/s3.tf
@@ -1,4 +1,9 @@
 resource "aws_s3_bucket" "default" {
-  bucket = "npd-test-tf-created"
+  bucket = "npd-test-tf-created-no3"
+  acl    = "private"
+}
+
+resource "aws_s3_bucket" "default2" {
+  bucket = "npd-test-tf-created-no2"
   acl    = "private"
 }
